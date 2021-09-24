239a4bd4d6e8b4510cef5a65fefc1faca77ae93b
Luis Arriojas <luisarriojas@gmail.com>
2021-09-24 14:15:51 -0400

    repo cleaned. Basic routes created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksListComponent } from './components/bookmarks-list/bookmarks-list.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'bookmarks', children: [
+    {path: '', component: BookmarksListComponent},
+    {path: '**', pathMatch: 'full', redirectTo: ''}
+  ]},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
